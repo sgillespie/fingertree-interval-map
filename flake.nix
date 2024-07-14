b18d47f277c59fc89d55034a81d3e009573dbd27
Sean D Gillespie <sean@mistersg.net>
2024-07-13 21:47:04 -0400

    chore: Haskell/nix boilerplate

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, haskellNix, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system :
+      let
+        overlays = [ haskellNix.overlay ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+
+        project = pkgs.haskell-nix.cabalProject' {
+          src = ./.;
+          compiler-nix-name = "ghc910";
+        };
+
+        flake = project.flake {};
+      in
+        pkgs.lib.recursiveUpdate flake {
+          packages.default = flake.packages."interval-map:lib:interval-map";
+        });
+}
