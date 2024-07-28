fe3f57e0d5ec9d5a2e83daa59f422498f7a009ba
Sean D Gillespie <sean@mistersg.net>
2024-07-27 22:44:08 -0400

    test: Add a failing test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,25 @@
 module Main (main) where
 
+import Data.IntervalMap as IntervalMap
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "IntervalMap" $ do
+    -- All scopes nested
+    it "let f = ... where g = ... where h = ..." $ do
+      let f = IntervalMap.Interval 0 10 :: IntervalMap.Interval Int
+          g = IntervalMap.Interval 5 10
+          h = IntervalMap.Interval 7 10
+
+          intervals =
+            IntervalMap.insert f "f" $
+              IntervalMap.insert g "g" $
+                IntervalMap.insert h "h" $
+                  empty
+
+      shallowSearch intervals `shouldBe` [g]
+
+-- Not yet defined
+shallowSearch :: IntervalMap.IntervalMap k v -> [IntervalMap.Interval k]
+shallowSearch = undefined
